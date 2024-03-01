ed8045e6d22c12aaa48dcf75d14b0e25c47df45c
Ashok <velmaashok765@gmail.com>
2024-03-01 05:05:40 +0000

    Solution

--- server.js
+++ server.js
@@ -8,31 +8,27 @@ const dbPath = path.join(__dirname, "./myData.db");
 let db = null;
 const PORT = process.env.PORT || 5000;
 
-const data = [
-  "Ashok",
-  "Software Developer",
-  "Skills: HTML,CSS,Java script,SQL,Node js,python,C++",
-];
+const initializeDbServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(PORT, () => {
+      console.log("Server running at https://localhost:5000");
+    });
+  } catch (e) {
+    console.log(`DB Error ${e.message}`);
+    process.exit(1);
+  }
+};
 
-// const initializeDbServer = async () => {
-//   try {
-//     db = await open({
-//       filename: dbPath,
-//       driver: sqlite3.Database,
-//     });
-//     app.listen(PORT, () => {
-//       console.log("Server running at https://localhost:5000");
-//     });
-//   } catch (e) {
-//     console.log(`DB Error ${e.message}`);
-//     process.exit(1);
-//   }
-// };
-
-// initializeDbServer();
+initializeDbServer();
 
 app.get("/", async (req, res) => {
-  res.send(data);
+  const query = "select * from Sample";
+  const list = await db.all(query);
+  res.send(list);
 });
 
 console.log("running");
